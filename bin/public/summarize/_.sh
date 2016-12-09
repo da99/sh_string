ff90c3e62ae4801d9e4d9b60f091c7375a275d5c
da99 <i-hate-spam-1234567@mailinator.com>
2016-12-09 02:49:22 -0600

    Added: summarize

---
+++ bin/public/summarize/_.sh
@@ -0,0 +1,22 @@
+
+# === {{CMD}} 9 "very long string" -> "very long..."
+summarize () {
+  local +x TARGET="$1"; shift
+  local +x TEXT="$(echo -n $@ | tr '\t' ' ' | tr -s ' ')"
+  local +x COUNT="$(echo "$@" | wc -m)"
+
+  if [[ "$COUNT" -le "$TARGET" ]]; then
+    echo "$TEXT"
+    return
+  fi
+
+  local +x SHORT="${TEXT:0:$(($TARGET + 1))}"
+  local +x CHAR="${TEXT:$TARGET:$(($TARGET + 1))}"
+
+  if [[ "$CHAR" == ' ' ]]; then
+    echo -n ${SHORT}...
+  fi
+
+  echo -n $SHORT | rev | grep -Pzo '(?s)[[:space:]]+\K(.+)' | rev
+  echo -n ...
+} # === end function
